8b36ae367e6500fd1c1967335069e97150b1eae5
chenminhua <chenminhua@Apples-Mac-mini.local>
2016-01-08 11:29:10 +0800

    first commit

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/*get users listing*/
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next){
+    res.send('response with a resource');
+});
+
+module.exports = router;
+
